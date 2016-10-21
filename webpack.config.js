ca2bef16dfb61eaed9638b459dac84de27270632
sirisaacr <idramirezs10@gmail.com>
2016-10-21 19:43:28 +0000

    Webpack and gulp base files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*
+config for webpack. Will be used in
+the Gulpfile for building our app.
+Does not need gulp in order to do so,
+but we use gulp to orchestrate
+ */
+module.exports = {
+  output: {
+    filename: 'bundle.js'
+  },
+
+  devtool: 'sourcemap',
+
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'raw' },
+      { test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.css/, loader: 'style!css' },
+      { test: /\.js$/, loader: 'babel?stage=1', exclude: [/client\/lib/, /node_modules/, /\.spec\.js/] }
+    ]
+  },
+
+  stylus: {
+    use: [require('jeet')(), require('rupture')()]
+  }
+};
